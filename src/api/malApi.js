1b8ae045be2b286de49420dca824d2a3b8c367c2
imok-coding <imokissick@gmail.com>
2025-12-10 05:43:57 -0600

    Updates

--- src/api/malApi.js
+++
@@ -1,6 +0,0 @@
-export async function fetchAnimeList() {
-  const url = 'https://imokmalworker.imokissick.workers.dev/';
-  const res = await fetch(url);
-  if (!res.ok) throw new Error('Failed to fetch MAL list');
-  return res.json();
-}
